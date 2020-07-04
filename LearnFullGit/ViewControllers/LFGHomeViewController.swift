017529c47d7017e637908ef5f7d8b92a8f5bb89d
Prabhudev <prabhudev.kannadoss@ext.freshworks.com>
2020-07-04 17:21:28 +0530

    testing code added

--- LearnFullGit/ViewControllers/LFGHomeViewController.swift
+++ LearnFullGit/ViewControllers/LFGHomeViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class LFGHomeViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    
+    func alerrView(){
+        let alert = UIAlertController(title: "HomeView Alert", message: "Message", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+              switch action.style{
+              case .default:
+                    print("default")
+
+              case .cancel:
+                    print("cancel")
+
+              case .destructive:
+                    print("destructive")
+        }}))
+        self.present(alert, animated: true, completion: nil)
     }
     
     @IBAction func logoutBtnAction(_ sender: UIButton)
@@ -21,5 +39,8 @@ class LFGHomeViewController: UIViewController {
         LFGConstants.appDelegate.setUpInitialViewController()
     }
     
+    @IBAction func moveToHome(_ sender: Any) {
+        alerrView()
+    }
     
 }
--- LearnFullGit/ViewControllers/LFGSignInViewController.swift
+++ LearnFullGit/ViewControllers/LFGSignInViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class LFGSignInViewController: UIViewController {
     
+    @IBOutlet weak var welcomeLabel: UILabel!
     
     @IBOutlet weak var userNameTxtFld: UITextField!
     @IBOutlet weak var passwordTxtFld: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+         alerrView()
+    }
+    
+    func alerrView(){
+        let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+              switch action.style{
+              case .default:
+                    print("default")
+
+              case .cancel:
+                    print("cancel")
+
+              case .destructive:
+                    print("destructive")
+        }}))
+        self.present(alert, animated: true, completion: nil)
     }
     
     
@@ -25,4 +42,7 @@ class LFGSignInViewController: UIViewController {
         LFGConstants.appDelegate.setUpInitialViewController()
     }
     
+    @IBAction func forgotPasswordAction(_ sender: Any) {
+        self.alerrView()
+    }
 }
